c1993d8b2437e9ad8b973ff4bbbc0153ec12a215
SonJoseph <joeson34@cs.unc.edu>
2019-01-22 15:50:04 -0500

    more testing

--- app/apis/connectCloudApps.php
+++ app/apis/connectCloudApps.php
@@ -1,14 +1,14 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");  //IMPORTANT: the env MYSQL_SERVICE_HOST/PORT assumes your database service name is "MYSQL"
 $dbport = getenv("MYSQL_SERVICE_PORT");  //If your database service name is "FOO", then this would be "FOO_SERVICE_HOST" and "FOO_SERVICE_PORT"
-$dbuser = getenv("MYSQL_USER");
+$dbuser = getenv("MYSQL_USERNAME");
 $dbpwd = getenv("MYSQL_PASSWORD");
-$dbname = getenv("MYSQL_DATABASE");
+$dbname = getenv("MYSQL_DATABASE_NAME");
 
 printf($dbhost);
 printf($dbport);
 printf($dbuser);
-printf($dbpwd);
+printf("password: ".$dbpwd);
 printf($dbname);
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
