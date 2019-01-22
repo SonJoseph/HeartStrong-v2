9bc3aa91663770090213cd9475c1ca9e8069575f
SonJoseph <joeson34@cs.unc.edu>
2019-01-22 16:36:54 -0500

    phpMyAdmin configs

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -28,7 +28,8 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = getenv(strtoupper(str_replace('-', '_', getenv('DATABASE_SERVICE_NAME'))).'_SERVICE_HOST');
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
