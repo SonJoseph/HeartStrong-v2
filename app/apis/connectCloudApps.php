935586559d444bcedcfd8b627c5cb9c6057b3f59
SonJoseph <joeson34@cs.unc.edu>
2019-01-22 16:00:35 -0500

    maybe this will work

--- app/apis/connectCloudApps.php
+++ app/apis/connectCloudApps.php
@@ -1,9 +1,9 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");  //IMPORTANT: the env MYSQL_SERVICE_HOST/PORT assumes your database service name is "MYSQL"
 $dbport = getenv("MYSQL_SERVICE_PORT");  //If your database service name is "FOO", then this would be "FOO_SERVICE_HOST" and "FOO_SERVICE_PORT"
-$dbuser = getenv("MYSQL_USERNAME");
-$dbpwd = getenv("MYSQL_PASSWORD");
-$dbname = getenv("MYSQL_DATABASE_NAME");
+$dbuser = getenv("username");
+$dbpwd = getenv("password");
+$dbname = getenv("database_name");
 
 printf($dbhost);
 printf($dbport);
