b398024060f45455d993bbf37f7b86b817f4dae1
SonJoseph <joeson34@cs.unc.edu>
2019-01-24 16:58:20 -0500

    init branch

--- app/apis/connect.php
+++ app/apis/connect.php
@@ -1,4 +1,8 @@
 <?php
+/*
+Hi, this is the dev branch. 
+We will use this branch to connect to our local servers for testing purposes.
+*/
 $dbhost = getenv("MYSQL_SERVICE_HOST");  //IMPORTANT: the env MYSQL_SERVICE_HOST/PORT assumes your database service name is "MYSQL"
 $dbport = getenv("MYSQL_SERVICE_PORT");  //If your database service name is "FOO", then this would be "FOO_SERVICE_HOST" and "FOO_SERVICE_PORT"
 $dbuser = getenv("username");
